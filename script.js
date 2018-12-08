b9935111a896c2c384fc6a616d42799069c5ce22
Dario Forde <Dariofdirect@gmail.com>
2018-12-08 05:56:09 -0500

    changed the Ai to make it smarter

--- script.js
+++ script.js
@@ -1,43 +1,46 @@
 var origBoard;
-const huPlayer = "0";
-const aiPlayer = "X";
+const huPlayer = 'O';
+const aiPlayer = 'X';
 const winCombos = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 1, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [6, 4, 2]
-];
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[6, 4, 2]
+]
 
-const cells = document.querySelectorAll(".cell");
+const cells = document.querySelectorAll('.cell');
 startGame();
 
 function startGame() {
-  document.querySelector(".endgame").style.display = "none";
-  origBoard = Array.from(Array(9).keys());
-  for (var i =0; i < cells.length; i++) {
-      cells[i].innerText = '';
-      cells[i].style.removeProperty('background-color');
-      cells[i].addEventListener('click', turnClick, false);
-  }
+	document.querySelector(".endgame").style.display = "none";
+	origBoard = Array.from(Array(9).keys());
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].innerText = '';
+		cells[i].style.removeProperty('background-color');
+		cells[i].addEventListener('click', turnClick, false);
+	}
 }
 
 function turnClick(square) {
-    turn(square.target.id, huPlayer)
+	if (typeof origBoard[square.target.id] == 'number') {
+		turn(square.target.id, huPlayer)
+		if (!checkWin(origBoard, huPlayer) && !checkTie()) turn(bestSpot(), aiPlayer);
+	}
 }
 
 function turn(squareId, player) {
-    origBoard[squareId] = player;
-    document.getElementById(squareId).innerText = player;
-    let gameWon = checkWin(origBoard, player)
-    if (gameWon) gameOver (gameWon)
+	origBoard[squareId] = player;
+	document.getElementById(squareId).innerText = player;
+	let gameWon = checkWin(origBoard, player)
+	if (gameWon) gameOver(gameWon)
 }
 
 function checkWin(board, player) {
-	let plays = board.reduce((a, e, i) => 
+	let plays = board.reduce((a, e, i) =>
 		(e === player) ? a.concat(i) : a, []);
 	let gameWon = null;
 	for (let [index, win] of winCombos.entries()) {
